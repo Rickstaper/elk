aa007817311a32337eda2dfee379f76e2280d10c
AlexanderKarcheuski <karchevskji@gmail.com>
2022-05-31 10:21:43 +0300

    Add

---
+++ Cars/Cars.API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Cars.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace Cars.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IElasticClient _elasticClient;
+
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(
+        IElasticClient elasticClient, ILogger<ProductsController> logger)
+    {
+        _elasticClient = elasticClient;
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetProducts")]
+    public async Task<IActionResult> Get(string keyword)
+    {
+        var result = await _elasticClient.SearchAsync<Product>(
+            s => s.Query(
+                q => q.QueryString(
+                    d => d.Query('*' + keyword + '*')))
+            .Size(1000)
+            );
+
+        return Ok(result.Documents.ToList());
+    }
+
+    [HttpPost(Name = "AddProduct")]
+    public async Task<IActionResult> Post(Product product)
+    {
+        await _elasticClient.IndexDocumentAsync(product);
+        return Ok();
+    }
+}
